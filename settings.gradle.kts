504571849f7d2b489b06951aa840f959447c8e80
Joket_abc <15005771700@qq.com>
2023-10-13 20:16:37 +0800

    1.20.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,10 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
+}
+
 rootProject.name = "kiocg"
 
 include("kiocg-api", "kiocg-server")
